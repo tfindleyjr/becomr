717d811727ef858f09b68c8531a70f4125914799
tfindleyjr <105574193+tfindleyjr@users.noreply.github.com>
2026-08-27 13:49:59 -0400

    Load BECOMR readability styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import "./readability.css";
 import TutorialOverlay from "./TutorialOverlay";
 
 export const metadata: Metadata = {
